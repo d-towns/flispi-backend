2bbe3da73f91f216b345aa04378bc432ca11cd81
dt0wns <dennis@dennistowns.com>
2023-11-30 21:25:22 -0500

    fix

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,6 @@ const Sequelize = require("sequelize");
 
 
 
-export const sequelize = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.PROD_POSTGRESS_URL : process.env.POSTGRESS_URL,, {
+export const sequelize = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.PROD_POSTGRESS_URL : process.env.POSTGRESS_URL, {
     dialect: 'postgres'
   });
